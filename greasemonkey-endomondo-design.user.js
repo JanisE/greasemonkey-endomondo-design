0b49d0bb7a4dc577c0664775f2c53d86c3c6f486
Jānis Elmeris <janise@inbox.lv>
2015-12-13 15:04:35 +0200

    Add a back-reference to the repository.

--- greasemonkey-endomondo-design.user.js
+++ greasemonkey-endomondo-design.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name Greasemonkey EndomondoDesign
 // @namespace https://github.com/JanisE
+// @description Project URL: https://github.com/JanisE/greasemonkey-endomondo-design
 // @include *www.endomondo.com/*
 // @version 1
 // @grant none
